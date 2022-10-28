fee7b91c95f80721fa065d69296efdcc1069c43c
Артём Межинский <aswert092@gmail.com>
2022-10-28 19:38:24 +0300

    Добавьте файлы проекта.

---
+++ IUser/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthoriseXML
+{
+    public interface IUser
+    {
+        internal class User
+        {
+            string _name;
+            string _password;
+            DateTime _birthDate;
+            string Login { get => _name; set => _name = value; }
+            string Password { get => _password; set => _password = value; }
+            DateTime DateOfBirth { get => _birthDate; set => _birthDate = value; }
+            User(string name, string password, DateTime dateOfBirth)
+            {
+                Login = name;
+                Password = password;
+                DateOfBirth = dateOfBirth;
+            }
+        }
+
+    }
+
+}
